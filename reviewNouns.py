5aa7a5de9b297be7f3781644c461c3a0417d64d9
jakenelson1 <72100265+jakenelson1@users.noreply.github.com>
2020-12-03 14:54:09 -0500

    changed-review-file

--- reviewNouns.py
+++ reviewNouns.py
@@ -107,7 +107,7 @@ f = open('gsmwithasin.csv', newline='', encoding="utf8")
 gsmfeatures = [i for i in csv.DictReader(f, delimiter= ',')] #list of dict
 f.close()
 
-f = open('reviews.csv', newline='', encoding="utf8") 
+f = open('20191226-reviews.csv', newline='', encoding="utf8") 
 csvReviews = [i for i in csv.DictReader(f, delimiter= ',')] #list of dict
 f.close()
 
@@ -131,64 +131,3 @@ csvResults.writerow(fieldnames)
 csvResults.writerows(rowbuffer)
 
 resultFile.close()
-
-
-
-    
-
-
-
-
-
-'''
-#Variables used for implementation
-count = 0
-features = [["battery", "charge"],  ["camera", "photo"], ["storage"]]
-fieldnames = ["asin"]
-for word in features:
-    fieldnames.append(word[0])
-
-#Open both Reviews.csv and Output CSV File
-f = open('reviews.csv', newline='', encoding="utf8") #CSV Contains Reviews
-resultFile = open('results.csv', 'w', newline = '', encoding="utf8") #Empty CSV to input results
-reader = csv.DictReader(f, delimiter= ',')
-writer = csv.writer(resultFile)
-
-#Write column headers in output CSV File
-writer.writerow(fieldnames)
-
-#For each row in 'Reviews', we extract the features found and their sentiment polarity
-for i in reader:
-    body = i['body']
-    blob = TextBlob(body)
-    
-    if count == 100: #Used to limit only checking first 100 reviews
-        break
-    
-    for sentence in blob.sentences:    #Separates text into sentences
-        for listt in features:          #We check each feature noun found in 'features'
-            for word in listt:
-                if word in sentence.lower():
-                    #print(listt, word)
-                    
-                    #Create array same size of features (Convention for CSV File)
-                    row = []
-                    for j in range(len(features)):
-                        row.append("")
-                        
-                        
-                    asinValue = i['asin']                           #Extract ASIN 
-                    polarityValue = sentence.sentiment.polarity     #Extract Sentiment value for Feature Found
-                    
-                    row[0] = asinValue                  #Insert ASIN of phone
-                    index = listt.index(word)        #Find position of insertion for feature polarity 
-                    row.insert(index+1, polarityValue)  #Insert polarity value in correct position, +1 due to position of 'ASIN' already allocated
-                    writer.writerow(row)                #Write row to file
-                    
-    count += 1
-
-
-#Close Files
-f.close()
-resultFile.close()   
-'''
